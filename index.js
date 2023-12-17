8f5e653f08455e764d9cbfb8c0324dc34de3d779
ronok420 <shahariarronok41.com>
2023-12-17 13:47:10 +0600

    server port changed

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 7000;
+const port = process.env.PORT || 7000;
 const categories = require('./data/categories.json');
 const news = require('./data/news.json');
-const cors = require('cors');
+var cors = require('cors')
 
+
+// Example middleware to handle OPTIONS requests
+// app.options('*', cors());
 app.use(cors());
 
-// Handle preflight requests
-app.options('*', cors());
 
-app.use((req, res, next) => {
-  // Set the necessary headers for CORS
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-  // Continue to the next middleware
-  next();
-});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -55,3 +48,5 @@ app.get('/news/:id', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+
